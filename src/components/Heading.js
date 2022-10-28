9e921002c116418691e776ca41bcbdb3db994e6e
Omobayode6 <omobayode93@gmail.com>
2022-10-28 13:45:48 +0100

    develope mobile view

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,6 +4,7 @@ function Heading() {
   return (
     <div className='heading'>
       <img src={require ('../assets/profile__img.png')} alt="profile" id='profile__img'/>
+      <img src={require ('../assets/_Avatar share button.png')} className='share' alt="avatar share button"/>
       <h2 id='twitter'>Omobayode6</h2>
       <h2 id='slack'>Omobayode Festus Osinubi </h2>
     </div>
--- src/components/Links.js
+++ src/components/Links.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Links = ({linktrees}) => {
   return(
-    linktrees.map((linktree, key) =>{
-      return(
-        
+    <div className='link-section'>
+    {
+      linktrees.map((linktree, key) =>{
+        return(
           <button className='btn' id={key}><a href={linktree.link} target="_blank" rel="noopener noreferrer">{linktree.name}</a></button>
-        
-      )
-    })
+        )
+      })}
+    </div>
   )
 }
 
